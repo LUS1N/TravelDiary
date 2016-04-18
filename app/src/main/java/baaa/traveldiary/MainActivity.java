23657f89109d8a23ddc19f748b2d4fae8b9324bc
LUS1N <sinkus.lukas@gmail.com>
2016-04-18 15:07:50 +0200

    add note header and row layouts

--- app/src/main/java/baaa/traveldiary/MainActivity.java
+++ app/src/main/java/baaa/traveldiary/MainActivity.java
@@ -1,7 +1,10 @@
 package baaa.traveldiary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExpandableListView noteExpandable = (ExpandableListView) findViewById(R.id.NoteExpandableListView);
+
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
     }
 }
