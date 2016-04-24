2bec59402d97db2c3ff8dcc08158c5c88155b52e
LUS1N <sinkus.lukas@gmail.com>
2016-04-24 12:29:28 +0200

    again rename

--- app/src/main/java/baaa/traveldiary/Example_Constants.java
+++ app/src/main/java/baaa/traveldiary/Example_Constants.java
@@ -1,6 +1,6 @@
 package baaa.traveldiary;
 
-public class Constants
+public class Example_Constants
 {
     public static final String MY_IMGUR_CLIENT_ID = "";
     public static final String MY_IMGUR_CLIENT_SECRET = "";
